c0af282029b2b6a7dff2eced13b910baf53f9ab2
amitraw03 <99679149+amitraw03@users.noreply.github.com>
2024-07-11 12:11:34 +0530

    okayyy

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,36 @@
 import RestaurantCard from "./RestaurantCard ";   //importing default exports
 import reslist from "../utils/mockData";
+import { useState } from "react";  //importing usestate hook is similr importing named exports
+import reslist from "../utils/mockData";
+
 
 const Body = () => {
+    //Local State Variable
+    const [ListOfRestaurants, setListOfRestaurants]=useState(reslist);  //setListOfRestaurants para use to update/modifies state var(ListOfRestaurants)
+
     return (
         <div className="body">
-            <div className="search">
-                <input type="search" placeholder="Search Your Food"></input>
-                <button>search</button>
+            <div className="features">
+
+                <div className="top-res">
+                    <button className="res-btn" 
+                        onClick={() => {
+                        const filteredList=ListOfRestaurants.filter(
+                            (res) => res.info.avgRating > 4
+                        );
+                        setListOfRestaurants(filteredList);
+                    }}>Top Rated Restaurants</button>
+                </div>
+
+                <div className="search">
+                    <input type="search" placeholder="Search Your Food"></input>
+                    <button>search</button>
+                </div>
             </div>
             <div className="res-container">
                 {
-                    reslist.map((restaurant) => (
-                        <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+                    ListOfRestaurants.map((restaurant) => (
+                        <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                     ))
                 }
             </div>
