c38fc39abe40d627e2d415a65c4c0855cb36a638
amitraw03 <99679149+amitraw03@users.noreply.github.com>
2024-07-13 18:04:09 +0530

    ok

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -16,7 +16,7 @@ const Body = () => {
     }, []);
 
     const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.508975&lng=77.3155706&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
         const response = await data.json();
         // console.log(response); 
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -3,47 +3,66 @@ import { useParams } from "react-router-dom";
 import Shimmer from "./shimmer";
 import { MENU_URL } from "../utils/constants";
 
+
 const RestaurantMenu=() =>{
    const [resInfo,setResInfo]= useState(null);
+
 
    const {resId}=useParams();
    
     useEffect(()=>{
        fetchMenu();
+       
     },[]);
-
+    
+     
     const fetchMenu= async ()=>{
-        const data= await fetch(MENU_URL +resId);
+        const data= await fetch(MENU_URL+829742);
 
         const response=await data.json();
-
+      //   console.log(response);
+        
+        console.log(response); 
         setResInfo(response.data);
+        console.log(resInfo); 
+        
 
     };
 
-    if(resInfo===null) return <Shimmer/>;
+    if(resInfo===null) return <Shimmer/>
 
-    const{name , cuisines , costForTwoMessage}= resInfo?.cards[2]?.card?.card?.info;   //destructurising the object
+   //  const{name , cuisines , costForTwoMessage}= resInfo?.cards[2]?.card?.card?.info;   //destructurising the object
 
-    const {itemCards} =resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+   //  const {itemCards} =resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+   // const cardInfo = resInfo?.cards[2]?.card?.card?.info || {};
+   // const name = cardInfo.name || 'Default Name';
+   // const cuisines = cardInfo.cuisines || [];
+   // const costForTwoMessage = cardInfo.costForTwoMessage || 'N/A';
+ 
+   // const itemCards = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards || [];
 
     return(
-          <div className="menu">
-             <h1>{name}</h1>
-            <p>
-               {cuisines.join(",")} -{costForTwoMessage}
-            </p>
-              <h2>Menu:</h2>
-             <ul>
-                 {itemCards.map((item) =>(
-                  <li key={item.card.info.id}>
-                     {item.card.info.name} -{"Rs."}
-                     {item.card.info.price/100 || item.card.info.defaultPrice/100 } 
-                  </li>
-                 ))}
-             </ul>
-          </div>
+         //  <div className="menu">
+         //     <h1>{name}</h1>
+         //    <p>
+         //       {cuisines.join(",")} -{costForTwoMessage}
+         //    </p> 
+         //        <h2>Menu:</h2>
+         //     <ul>
+         //         {itemCards.map((item) =>(
+         //          <li key={item.card.info.id}>
+         //             {item.card.info.name} -{"Rs."}
+         //             {item.card.info.price/100 || item.card.info.defaultPrice/100 } 
+         //          </li>
+         //         ))}
+         //     </ul>
+         //  </div> 
+         
+        <div></div>
     );
 };
+
+
 
 export default RestaurantMenu;
