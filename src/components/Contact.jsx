a061e49b2a4af4be6b601b10f600d7dec515fd71
amitraw03 <99679149+amitraw03@users.noreply.github.com>
2024-07-22 14:23:53 +0530

    unit testing started

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
   return (
     <div>
         <h1>Contact Us</h1>
-        <h2>Thanks For Reaching Out to Us....</h2>
+        {/* <h2>Thanks For Reaching Out to Us....</h2> */}
     </div>
 
   );
